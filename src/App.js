f66ce336c6986b71a0f688b6f79d57921ce17e72
Niels Koster <niels.koster@portavita.nl>
2020-10-21 14:46:58 +0200

    Todo component skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>TO DO APP</p>
+        <Todo />
       </header>
     </div>
   )
