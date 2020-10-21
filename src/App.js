73bfee516280818ba68793bc9a81e73aa783f3cf
Niels Koster <niels.koster@portavita.nl>
2020-10-21 14:45:53 +0200

    Initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>TO DO APP</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
